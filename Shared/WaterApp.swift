7e327d75dc2693d28ed692850565d294220e4334
Nino <Porfirio.ninoiii@gmail.com>
2021-08-21 14:14:06 -0500

    Initial Commit

---
+++ Shared/WaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterApp.swift
+//  Shared
+//
+//  Created by App Creation on 8/21/21.
+//
+
+import SwiftUI
+
+@main
+struct WaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
